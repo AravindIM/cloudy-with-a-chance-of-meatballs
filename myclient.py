5b1f225b73e6624ac1756ff5fc231199f9cc0795
Aravind I M <aravindim@yahoo.com>
2022-11-12 01:03:35 +0530

    Added: Autoscaler and autoscaling communication to client

--- myclient.py
+++ myclient.py
@@ -6,6 +6,7 @@ import socket
 import sys
 from threading import Thread
 import random
+import time
 
 
 SERVER_IP = "192.168.122.2"
@@ -13,6 +14,7 @@ SERVER_PORT = 8008
 BUFFER_SIZE = 2048
 
 ips = [SERVER_IP]
+
 
 class Client:
     """Create the client and functions to connect with server."""
@@ -25,7 +27,7 @@ class Client:
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self):
-        """Connect to the lient object with host, and port."""
+        """Connect to the client object with host, and port."""
         self._socket.connect((self.host, self.port))
 
     def request(self, message):
@@ -41,26 +43,31 @@ def handle_connected(thread_num, busy_time):
     """Send request and recieve response from server."""
     global ips
     while True:
+        time.sleep(1)
         try:
             client = Client(random.choice(ips), SERVER_PORT, BUFFER_SIZE)
             client.connect()
-            print(f"[Thread{thread_num}]: Connecting to server {client.host} [OK]")
+            print(f"[Thread{thread_num}]: Connecting to server {client.host} ... [OK]")
             client.request(busy_time)
-            print(f"[Thread{thread_num}]: Sending message to server {client.host} [OK]")
+            print(f"[Thread{thread_num}]: Sending message to server {client.host} ... [OK]")
             response = client.response()
-            print(f"[Thread{thread_num}]: Recieved response from server {client.host} [OK]")
+            print(f"[Thread{thread_num}]: Recieved response from server {client.host} ... [OK]")
             print(f"[Thread{thread_num}]: Response from {client.host}: {response}")
         except Exception as e:
             print(f"ERROR: {e}")
 
+
 def handle_autoscaler_connection():
+    """Recieve information from autoscaler."""
     global ips
     autoscaler_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    autoscaler_socket.bind(("127.0.0.1",6009))
+    autoscaler_socket.bind(("127.0.0.1", 6009))
     autoscaler_socket.listen(10)
     conn, _ = autoscaler_socket.accept()
     new_ip = conn.recv(BUFFER_SIZE).decode()
+    print(f"[AUTOSCALER]: Recieved new ip: {new_ip}")
     ips.append(new_ip)
+
 
 def main():
     """Run the code."""
@@ -73,7 +80,7 @@ def main():
             clientThread = Thread(target=handle_connected, args=(i+1, busy_time))
             clientThread.start()
     except Exception as error:
-        print(f"Error parsing command line arguments : {error}")
+        print(f"ERROR: Please type thread count and busy time as arguments. {error}")
 
 
 if __name__ == "__main__":
